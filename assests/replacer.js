bc11789224c78ad3d25132e5bbae21c896c21111
Hugwalk <extended.wl@gmail.com>
2019-09-03 01:18:40 +0800

    Initial commit
    i hate git commit history

---
+++ assests/replacer.js
@@ -0,0 +1,47 @@
+const qs = document.querySelector.bind(document);
+
+const copied = document.querySelector('#copied');
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+function ifEmpty(input, fb) {
+    return input ? `${input}` : fb;
+}
+
+function doReplace() {
+    const device_name = qs('#device_name').value;
+    const device_codename = qs('#device_codename').value;
+    const name = qs('#name').value;
+    const bugs = ifEmpty(qs('#bugs').value, 'You tell me');
+    const screenshot_album_url = ifEmpty(qs('#screenshot_album_url').value, 'https://imgur.com/a/6l0dMag');
+    const donate_url = ifEmpty(qs('#donate_url').value, 'https://google.com');
+    const xda_url = qs('#xda_url').value;
+    const kernel_source_url = qs('#kernel_source_url').value;
+
+    qs('#output').value =
+        template.replace(/##DEVICE_NAME##/g, device_name)
+        .replace(/##DEVICE_CODENAME##/g, device_codename)
+        .replace(/##NAME##/g, name)
+        .replace(/##BUGS##/g, bugs)
+        .replace(/##SCREENSHOT_ALBUM_URL##/g, screenshot_album_url)
+        .replace(/##DONATE_URL##/g, donate_url)
+        .replace(/##XDA_URL##/g, xda_url)
+        .replace(/##KERNEL_SOURCE_URL##/g, kernel_source_url)
+}
+
+// qs('#generate').addEventListener('click', async() => {
+
+// });
+
+qs('#form').addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    doReplace();
+    copied.style.opacity = 1;
+    document.getElementById('output').select();
+    document.execCommand('copy');
+    document.getElementById('copied').innerHTML = 'Copied';
+    delay(2000).then(() => {
+        copied.style.opacity = 0;
+    });
+});
---
+++ assests/template.js
@@ -0,0 +1,139 @@
+var template = `[CENTER]
+[img]https://i.imgur.com/6LY4JxC.png[/img]
+[SIZE="5"][b][color=#0060FF]Evolution X 2.0 for ##DEVICE_NAME## [##DEVICE_CODENAME##][/color][/b][/SIZE]
+
+
+[img]https://i.imgur.com/dtheyqU.png[/img]
+[CENTER][SIZE="3"]Pixel UI, Android Q vibes, Customization and more.
+Brought to you by [url="https://t.me/stallix"]Stallix[/url], [url="https://t.me/realakito"]RealAkito[/url], [url="https://t.me/anierinb"]AnierinB[/url] and [url="https://t.me/hugwalk"]Hugwalk[/url].
+[B]This is Evolution X. [COLOR=#0060FF]#KeepEvolving[/COLOR][/B][/SIZE][/CENTER]
+
+
+[img]https://i.imgur.com/t98eWHY.png[/img]
+[HIDE][SIZE="3"][COLOR=#70B8FF][B]Statusbar[/B][/COLOR][/SIZE]
+Battery Icon Customization
+Clock Fonts & Colors
+Evolution X Icon Toggle (39 Styles)
+Notification Ticker
+Roaming Icon Toggle
+VoLTE & 4G/LTE Icon toggle
+
+[SIZE="3"][COLOR=#70B8FF][B]Navigation[/B][/COLOR][/SIZE]
+Extra Button Customization
+Invertable Layout
+Navigation Gestures
+One-Handed Mode
+Pixel Animation Toggle
+Portrait/Landscape Height/Width Configurations
+Pulse (Navigation Bar Audio Visualization)
+
+[SIZE="3"][COLOR=#70B8FF][B]Quick Settings[/B][/COLOR][/SIZE]
+Animations (Flip/Rotate)
+Bottom Brightness Slider Toggle
+Edit/Running Services Icon Toggle
+Panel Colors (RGB picker)
+Panel Opacity Selector
+Quick Pulldown (Right/Left)
+Rows & Columns Customization
+Styles (26 Styles)
+Tile Titles Toggle
+
+[SIZE="3"][COLOR=#70B8FF][B]Lockscreen[/B][/COLOR][/SIZE]
+Clock Font Sizes & Colors
+Clock Styles (15 Styles)
+Date/Alarms/Zen Hider
+Status Bar Icons Toggle
+
+[SIZE="3"][COLOR=#70B8FF][B]Ambient Display[/B][/COLOR][/SIZE]
+Ambient Display Gestures (Hand Wave, etc.)
+Music Ticker & Visualization Options
+
+[SIZE="3"][COLOR=#70B8FF][B]Themes[/B][/COLOR][/SIZE]
+Accent's (43 styles, Including 6 Q accents)
+Font Picker
+Rootless Substratum Support
+Wallpaper/Light/Dark switching
+
+[SIZE="3"][COLOR=#70B8FF][B]Buttons and Gestures[/B][/COLOR][/SIZE]
+AOSP & Fingerprint Gestures
+Power Menu Customization
+Volume Key Cursor Control
+Volume Key Playback Control
+Volume Panel Placement (Right/Left)
+
+[SIZE="3"][COLOR=#70B8FF][B]Notifications[/B][/COLOR][/SIZE]
+Blink Flashlight On Call
+Call Vibrations
+Heads Up Notification & less Annoying Toggle
+Kill App Button
+Notification Timeout
+Q Style Seek Bar
+Smart Notification Sounds
+Snooze Timer
+Stoplist & Blacklist for Heads up
+
+[SIZE="3"][COLOR=#70B8FF][B]Miscellaneous[/B][/COLOR][/SIZE]
+Alarm Blocker
+Default USB Config
+Doze Settings & Profiles
+Gaming Mode
+Sensor Block Per Package
+Signature Spoofing Support
+Smart Charging
+Time In State Monitor
+Wake On Plug
+Wakelock Blocker
+[/HIDE]
+
+
+[img]https://i.imgur.com/6FbhXUQ.png[/img]
+[url="##SCREENSHOT_ALBUM_URL##"][b][color=#0060FF][u][SIZE="5"]View Screenshots[/SIZE][/u][/color][/b][/url]
+
+
+[img]https://i.imgur.com/afrUQ79.png[/img]
+[LIST]
+[*]##BUGS##
+[/LIST]
+
+[SIZE="3"][B]Do NOT flash GApps, already included in the rom.[/B][/SIZE]
+
+
+[img]https://i.imgur.com/jkvuFLT.png[/img]
+[LIST=1]
+[*]Reboot to Recovery
+[*]Download Evolution X
+[*]Wipe Data/Cache/System
+[*]Flash the latest build
+[*]Flash TWRP Installer ZIP
+[*]Reboot to System
+[*]Enjoy and [COLOR=#0060FF]#KeepEvolving[/COLOR]
+[/LIST]
+
+
+[img]https://i.imgur.com/5x1I88i.png[/img]
+[SIZE="5"][url="https://sourceforge.net/projects/evolution-x/files/##DEVICE_CODENAME##/"][b][color=#0060FF][u]DOWNLOAD ON SOURCEFORGE[/u][/color][/b][/url][/SIZE]
+
+
+
+[img]https://i.imgur.com/XQZkdk7.png[/img]
+[SIZE="3"][b][color=#0060FF]Like Evolution X? Please consider donation. Thanks![/color][/b][/SIZE]
+[url="##DONATE_URL##"][color=#70B8FF][u]Donate ##NAME## (Device Maintainer)[/u][/color][/url]
+[url="https://www.paypal.me/joeyhuab"][color=#70B8FF][u]Donate Joey Huab (ROM Developer)[/u][/color][/url]
+
+
+[SIZE="3"][b][color=#0060FF]Supports[/color][/b][/SIZE]
+[url="https://t.me/EvolutionXOfficial"][color=#70B8FF][u]Join Our Channel for Updates![/u][/color][/url]
+[url="https://t.me/EvolutionX"][color=#70B8FF][u]Join Our Official Chat![/u][/color][/url]
+[url="https://t.me/EvolutionX##DEVICE_CODENAME##"][color=#70B8FF][u]Join Support Chat for ##DEVICE_NAME##![/u][/color][/url]
+
+
+
+[img]https://i.imgur.com/1AUPwTl.png[/img][/CENTER]
+[LEFT][SIZE="3"]
+[b]Android OS version:[/b] 9.0.0_r40
+[b]Security patch level:[/b] Month YYYY
+[b]Build author:[/b] [url=##XDA_URL##]##NAME##[/url]
+[b]Source code:[/b] [url=https://github.com/Evolution-X][url]https://github.com/Evolution-X[/url][/url]
+[b]Kernel Source code:[/b] [url=##KERNEL_SOURCE_URL##]##KERNEL_SOURCE_URL##[/url]
+[b]ROM Developer:[/b] [url=https://forum.xda-developers.com/member.php?u=4936496]Joey Huab[/url]
+[/SIZE][/LEFT]`;

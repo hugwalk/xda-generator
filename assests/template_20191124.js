8b27fbef980354036c7e911df0c3007dd2c06615
Hugwalk <extended.wl@gmail.com>
2019-11-24 23:01:17 +0800

    Update template

---
+++ assests/template_20191124.js
@@ -0,0 +1,111 @@
+var template = `[CENTER]
+[IMG]https://github.com/Evolution-X/platform_manifest/raw/pie/EvoBanner.png[/IMG]
+[SIZE="5"][b][color=#0060FF]Evolution X 3.3 for the ##DEVICE_NAME## [##DEVICE_CODENAME##][/color][/b][/SIZE]
+ 
+
+[img]https://i.imgur.com/qE4kqsj.png[/img]
+[SIZE="3"]Pixel UI, Customization and More. We Are Evolution X![/SIZE]
+
+[img]https://i.imgur.com/RgqrlpK.png[/img]
+[HIDE]
+[SIZE="2"][U][I][COLOR="#0060FF"][B]Themes[/B][/COLOR][/I][/U][/SIZE]
+- 20 Accent Colors
+- 5 Lock Screen Clock Styles
+- Pixel 2019 Wallpapers
+- Pixel Theme Picker
+[SIZE="2"][U][I][COLOR="#0060FF"][B]Statusbar[/B][/COLOR][/I][/U][/SIZE]
+- 4G/LTE Icon Toggle
+- Brightness Control
+- Network Traffic Activity
+- VoLTE Toggle
+[SIZE="2"][U][I][COLOR="#0060FF"][B]Notifications[/B][/COLOR][/I][/U][/SIZE]
+- Force Close Notifications
+- In Call Vibrations
+- Kill App Button
+- Notification light Config
+[SIZE="2"][U][I][COLOR="#0060FF"][B]Quick Settings[/B][/COLOR][/I][/U][/SIZE]
+- Bottom Brightness Slider Toggle
+- Gaming Mode Quick Settings Tile
+- Quick Pulldown (Left/Right)
+- Quick Settings Rows & Columns Customization
+- Tile Titles Toggle
+[SIZE="2"][U][I][COLOR="#0060FF"][B]Gestures[/B][/COLOR][/I][/U][/SIZE]
+- AOSP Gestures
+- Back Gesture Height Config
+- Double Tap to Sleep Lockscreen
+- Double Tap To Sleep Status Bar
+- Screen Off Power Button Torch
+- Swipe To Screenshot
+[SIZE="2"][U][I][COLOR="#0060FF"][B]Lockscreen[/B][/COLOR][/I][/U][/SIZE]
+- Fingerprint Authentication Vibration Toggle
+- Lock Screen Charging Info
+- Lock Screen Music Visualizer
+- Lock Screen Shortcuts
+- Media Artwork Toggle
+- Pocket Detection
+- Status bar visibility toggle
+[SIZE="2"][U][I][COLOR="#0060FF"][B]Navigation[/B][/COLOR][/I][/U][/SIZE]
+- Invert Layout
+[SIZE="2"][U][I][COLOR="#0060FF"][B]Hardware Buttons[/B][/COLOR][/I][/U][/SIZE]
+- Power Menu Customization
+- Volume Rocker Cursor Control
+- Volume Rocker Playback Control
+- Volume Rocker Wake
+[SIZE="2"][U][I][COLOR="#0060FF"][B]Miscellaneous[/B][/COLOR][/I][/U][/SIZE]
+- Alarm Blocker
+- Default USB Config
+- Wakelock Blocker
+- Wake On Charge[/HIDE]
+
+[img]https://i.imgur.com/n6b8AXD.png[/img]
+[SIZE="3"]##BUGS##[/SIZE]
+ 
+[SIZE="3"][B]DO NOT FLASH GAPPS, ALREADY INCLUDED[/B][/SIZE]
+
+[img]https://i.imgur.com/YtvYmxb.png[/img]
+1. Reboot to Recovery
+2. Download the proper flashable ZIP for your device
+3. Wipe Data/Cache/System when coming from non-AOSP ROMs
+4. Flash the ROM
+5. Flash TWRP installer ZIP
+6. Reboot to System
+7. Reboot to Recovery
+8. Flash Magisk (Optional)
+9. Reboot to System
+
+[URL="##SCREENSHOT_ALBUM_URL##"][img]https://i.imgur.com/3EYXY4z.png[/img][/URL]
+[URL="https://sourceforge.net/projects/evolution-x/files/##DEVICE_CODENAME##"][img]https://i.imgur.com/UfrpkUs.png[/img][/URL]
+
+[URL="##DONATE_URL##"][img]https://i.imgur.com/nkJHit6.png[/img][/URL][URL="https://t.me/EvolutionX"][img]https://i.imgur.com/AteOLUU.png[/img][/URL][URL="https://t.me/EvolutionX##DEVICE_CODENAME##"][img]https://i.imgur.com/8cQbEUV.png[/img][/URL]
+ 
+[hr][/hr]
+
+[img]https://i.imgur.com/LU9gsrQ.png[/img][/CENTER]
+[LEFT][SIZE="3"][list]
+[*][b]Android OS version:[/b] 10.0_r14
+[*][b]Security patch level:[/b] November 2019
+[*][b]Build author:[/b] [url="##XDA_URL##"]##NAME##[/url]
+[*][b]Source code:[/b] [url="https://github.com/EvoX-Temp"][url]https://github.com/EvoX-Temp[/url][/url]
+[*][b]Kernel Source code:[/b] [url="##KERNEL_SOURCE_URL##"][url]##KERNEL_SOURCE_URL##[/url][/url]
+[*][b]ROM Developer:[/b] [url="https://forum.xda-developers.com/member.php?u=4936496"]joeyhuab[/url]
+[/list][/SIZE][/LEFT]
+ 
+[B][U][SIZE="4"][NOPARSE]XDA:DevDB Information[/NOPARSE][/SIZE][/U][/B]
+[SIZE=3][B]Evolution X 3.3, ROM for the ##DEVICE_NAME##[/B][/SIZE]
+ 
+[B][U]Contributors[/U][/B]
+[URL="https://forum.xda-developers.com/member.php?u=4936496"]joeyhuab[/URL], [URL="https://forum.xda-developers.com/member.php?u=7125966"]AnierinB[/URL], [URL="https://forum.xda-developers.com/member.php?u=9008281"]peaktogoo[/URL], [URL="https://forum.xda-developers.com/member.php?u=7665334"]hugwalk[/URL]
+B]Source Code:[/B] [URL="https://github.com/Evolution-X"][url]https://github.com/Evolution-X[/url][/URL]
+ 
+[B]ROM OS Version:[/B] 10.0 Ten
+[B]ROM Kernel:[/B] Linux 4.x
+[B]ROM Firmware Required:[/B] Unlocked Bootloader, TWRP/SHRP
+[B]Based On:[/B] AOSP
+ 
+[B][U]Version Information[/U][/B]
+[B]Status:[/B] Stable
+[B]Current Stable Version:[/B] [url]https://sourceforge.net/projects/evolution-x/files/##DEVICE_CODENAME##[/url]
+[B]Stable Release Date:[/B] 
+ 
+[B]Created[/B] 
+[B]Last Updated[/B] `;

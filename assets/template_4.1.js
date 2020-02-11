058bf7a763bb390ddcde2b2a298e9b2ad4d23cbd
hugwalk <extended.wl@gmail.com>
2020-02-11 22:30:28 +0800

    Update for 4.1

---
+++ assets/template_4.1.js
@@ -0,0 +1,165 @@
+var template = `[CENTER]
+[IMG]https://raw.githubusercontent.com/Evolution-X/manifest/ten/EvoBanner.png[/IMG]
+[SIZE="5"][b][color=#0060FF]Evolution X 4.0 for ##DEVICE_NAME## [##DEVICE_CODENAME##][/color][/b][/SIZE]
+
+[CODE]/*
+* Your warranty is void. Or vaild, probably?
+*
+* I am not responsible for bricked devices, dead SD cards, Ebolation X,
+* thermonuclear war, or the current economic crisis caused by you following 
+* these directions. YOU are choosing to make these modificiations, and if 
+* you point your finger at me for messing up your device, I will LMAO at you.
+*/[/CODE]
+
+[img]https://i.imgur.com/AzBiTvF.png[/img]
+[SIZE="5"][COLOR="#0060FF"][B]Living, Breaking, Keep Evolving. [/B][/COLOR][/SIZE]
+
+[SIZE="3"][B]Evolution X[/B] is a flashable custom ROM to bring a true Pixel feel to your Android Device at first glance, with many additional configurations at your disposal. We aim to provide frequent builds with monthly security patches from the latest aosp sources![/SIZE]
+
+[B][color=#0060FF]- Team Evolution X - [/color][/B]
+@[URL="https://forum.xda-developers.com/member.php?u=4936496"]joeyhuab[/URL] (Founder & Lead Developer)
+@[URL="https://forum.xda-developers.com/member.php?u=9008281"]peaktogoo[/URL] (Project Manager)
+@[URL="https://forum.xda-developers.com/member.php?u=7125966"]AnierinB[/URL] (Project Specialist)
+
+[B][color=#0060FF]- Design - [/color][/B]
+@[URL="https://forum.xda-developers.com/member.php?u=7665334"]Hugwalk[/URL] (Lead Designer)
+@[URL="https://forum.xda-developers.com/member.php?u=10435351"]ecnivtwelve[/URL] (Designer)
+@[URL="https://forum.xda-developers.com/member.php?u=9633220"]MartonDev[/URL] (Website Designer)
+
+https://evolution-x.org/
+
+[I]Reach us on Twitter! [URL="https://twitter.com/EvolutionXROM"]@EvolutionXROM[/URL][/I]
+
+[img]https://i.imgur.com/WAdbfjy.png[/img]
+[HIDE]
+[SIZE="3"][U][I][COLOR="#0060FF"][B]Themes[/B][/COLOR][/I][/U][/SIZE]
+- 5 Lock Screen Clock Styles
+- 6 System Icon Shapes
+- 17 System Fonts
+- Pixel 2019 Wallpapers
+- RGB Accent Picker
+[SIZE="3"][U][I][COLOR="#0060FF"][B]Statusbar[/B][/COLOR][/I][/U][/SIZE]
+- 4G/LTE Icon Toggle
+- 7 Battery Icon Styles
+- 7 Custom VoLTE Icons
+- Battery Percent Show/Hide Config
+- Brightness Control
+- Carrier Label Settings
+- Clock Options
+- Network Traffic Activity
+- Rounded Corners Config
+[SIZE="3"][U][I][COLOR="#0060FF"][B]Notifications[/B][/COLOR][/I][/U][/SIZE]
+- Blink Flashlight On Call
+- Edge Lighting
+- Force Close Notifications
+- In Call Vibrations
+- Kill App Button
+- Notification light Config
+[SIZE="3"][U][I][COLOR="#0060FF"][B]Quick Settings[/B][/COLOR][/I][/U][/SIZE]
+- Battery Percentage Configs
+- Bottom Brightness Slider Toggle
+- Custom QS Footer Text
+- Custom QS Header Images
+- Gaming Mode Quick Settings Tile
+- QS Backround Image Blur
+- QS Opacity & Color Configs
+- Quick Pulldown (Left/Right)
+- Quick Settings Rows & Columns Customization
+- Tile Titles Toggle
+- Toggles To Hide Misc Icons
+[SIZE="3"][U][I][COLOR="#0060FF"][B]Gestures[/B][/COLOR][/I][/U][/SIZE]
+- AOSP Gestures
+- Back Gesture Height Config
+- Double Tap to Sleep Lockscreen
+- Double Tap To Sleep Status Bar
+- Screen Off Gestures
+- Screen Off Power Button Torch
+- Swipe To Screenshot
+- Toggle To Hide Navbar Pill
+[SIZE="3"][U][I][COLOR="#0060FF"][B]Lockscreen[/B][/COLOR][/I][/U][/SIZE]
+- Custom Clock & Date Fonts
+- Fingerprint Authentication Vibration Toggle
+- FOD Icon Picker
+- Lock Screen Music Visualizer
+- Lock Screen Media Artwork Configs
+- Lock Screen Shortcuts
+- Pocket Detection
+- Pressed FOD Icon Picker
+- Status bar visibility toggle
+[SIZE="3"][U][I][COLOR="#0060FF"][B]Hardware Buttons[/B][/COLOR][/I][/U][/SIZE]
+- Power Menu Customization
+- Volume Rocker Cursor Control
+- Volume Rocker Playback Control
+- Volume Rocker Wake
+[SIZE="3"][U][I][COLOR="#0060FF"][B]Animations[/B][/COLOR][/I][/U][/SIZE]
+- Disable Transition Animations
+- ListView Animations
+- QS Tile Animation Duration & Interpolator configs
+- QS Tile Animations (Flip/Rotate)
+- Screen Off Animations
+- Scrolling Cache Configs
+- System Animation Configs
+[SIZE="3"][U][I][COLOR="#0060FF"][B]Navigation[/B][/COLOR][/I][/U][/SIZE]
+- Invert Layout
+- Navigation Bar Hide/Show Toggle
+- Navigation Bar Tuner
+[SIZE="3"][U][I][COLOR="#0060FF"][B]Miscellaneous[/B][/COLOR][/I][/U][/SIZE]
+- Accidental Wake Up Prevention
+- Alarm Blocker
+- Default USB Config
+- Doze Settings
+- Gaming Mode
+- Legacy App Scalling
+- Sensor Block Per Package
+- Signature Spoofing Support
+- Smart Charging
+- Time In State Monitor
+- Wakelock Blocker
+- Wake On Charge[/HIDE]
+
+[img]https://i.imgur.com/A5dQFGK.png[/img]
+##BUGS##
+ 
+[SIZE="3"][B]DO NOT FLASH GAPPS, ALREADY INCLUDED[/B][/SIZE]
+
+[img]https://i.imgur.com/gMGb8ee.png[/img]
+[SIZE="5"][B][COLOR="#0060FF"]First Time Install / Clean Flash[/COLOR][/B][/SIZE]
+1. Download the proper flashable ZIP for your device
+2. Reboot to Recovery
+3. Wipe Data/Cache/System
+4. Format Data (Optional)
+5. Flash the ROM
+6. Reboot to System
+7. Reboot to Recovery (Optional)
+8. Flash Magisk (Optional)
+9. Reboot to System and [COLOR="#0060FF"]#KeepEvolving[/COLOR]
+
+[SIZE="5"][B][COLOR="#0060FF"]Update / Dirty Flash[/COLOR][/B][/SIZE]
+1. Reboot to Recovery
+2. Download the proper flashable ZIP for your device
+3. Reboot to Recovery (Optional)
+4. Flash Magisk (Optional)
+5. Flash the ROM
+6. Reboot to System and [COLOR="#0060FF"]#KeepEvolving[/COLOR]
+
+[URL="https://imgur.com/gallery/JjMQ5s4"][img]https://i.imgur.com/lOTsdPJ.png[/img][/URL][URL="https://imgur.com/gallery/IEX8XPC"][img]https://i.imgur.com/lkbZagx.png[/img][/URL]
+
+[URL="https://sourceforge.net/projects/evolution-x/files/##DEVICE_CODENAME##"][img]https://i.imgur.com/BsqMW2B.png[/img][/URL]
+[URL="##DONATE_URL##"][img]https://i.imgur.com/JBON20U.png[/img][/URL][URL="https://t.me/EvolutionX"][img]https://i.imgur.com/9dKRGuj.png[/img][/URL][URL="https://t.me/EvolutionX##DEVICE_CODENAME##"][img]https://i.imgur.com/PZ6h5NN.png[/img][/URL]
+
+[SIZE="1"][URL="https://imgur.com/gallery/JjMQ5s4"]Light Screenshots[/URL] / [URL="https://imgur.com/gallery/IEX8XPC"]Dark Screenshots[/URL] / [URL="https://sourceforge.net/projects/evolution-x/files/##DEVICE_CODENAME##"]Download[/URL]
+[URL="##DONATE_URL##"]Donate me![/URL] / [URL="https://t.me/EvolutionX"]Official Chat[/URL] / [URL="https://t.me/EvolutionX##DEVICE_CODENAME##"]Device Support[/URL][/SIZE]
+
+[hr][/hr]
+
+[img]https://i.imgur.com/NzyZaVK.png[/img][/CENTER]
+[LEFT][list]
+[*][b]Evolution X Version:[/b] 4.1
+[*][b]Android OS Version:[/b] 10.0_r27
+[*][b]Security Patch Level:[/b] Febuary 2020
+[*][b]Build Author:[/b] [url="##XDA_URL##"]##NAME##[/url]
+[*][b]Source Code:[/b] [url="https://github.com/Evolution-X"]https://github.com/Evolution-X[/url]
+[*][b]Kernel Source Code:[/b] [url="##KERNEL_SOURCE_URL##"]##KERNEL_SOURCE_URL##[/url]
+[*][b]ROM Developer:[/b] [url="https://forum.xda-developers.com/member.php?u=4936496"]joeyhuab[/url]
+[/list][/LEFT]
+`;

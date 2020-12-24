8cd61f2046c070c18055b8e081a006c3f11bfcea
Akito Mizukito <akito@evolution-x.org>
2020-12-24 22:55:46 +0900

    Add Evolution X 5.2
    Change-Id: Ie371850f8cae45b114cf5b7de84b81e4e59de7ef
    Signed-off-by: Akito Mizukito <akito@evolution-x.org>

---
+++ assets/template_5.x.js
@@ -0,0 +1,118 @@
+var template = `
+[CENTER]
+
+
+[IMG]https://raw.githubusercontent.com/Evolution-X/manifest/elle/EvoBanner.png[/IMG]
+
+[SIZE=5][B][COLOR=#0060FF]Evolution X 5.2 for the ##DEVICE_NAME## [##DEVICE_CODENAME##][/COLOR][/B][/SIZE]
+
+[CODE]/*
+
+* Your warranty is void. Or vaild, probably?
+
+*
+
+* I am not responsible for bricked devices, dead SD cards, Ebolation X,
+
+* thermonuclear war, or the current economic crisis caused by you following
+
+* these directions. YOU are choosing to make these modificiations, and if
+
+* you point your finger at me for messing up your device, I will LMAO at you.
+
+*/[/CODE]
+
+
+
+[IMG]https://i.imgur.com/qE4kqsj.png[/IMG]
+
+[SIZE=5][COLOR=#0060FF][B]Living, Breaking, Keep Evolving. [/B][/COLOR][/SIZE]
+
+[SIZE=3]Pixel UI, customization and more, we are Evolution X!
+
+
+
+[B]- Team Evolution X - [/B]
+
+@[URL='https://forum.xda-developers.com/member.php?u=4936496']joeyhuab[/URL]
+
+@[URL='https://forum.xda-developers.com/member.php?u=9008281']peaktogoo (Now RealAkito)[/URL]
+
+
+
+[I]Reach us on Twitter! [URL='https://twitter.com/EvolutionXROM']@EvolutionXROM[/URL][/I][/SIZE]
+
+
+
+
+[IMG]https://i.imgur.com/RgqrlpK.png[/IMG]
+
+[HIDE]
+
+[SIZE=2][U][I][COLOR=#0060FF][B]What are our features?[/B][/COLOR][/I][/U][/SIZE]
+
+- Just flash and check "The Evolver"[/HIDE]
+
+
+
+[IMG]https://i.imgur.com/n6b8AXD.png[/IMG]
+##BUGS##
+
+[SIZE=3][B][COLOR=red]DO NOT FLASH GAPPS, ALREADY INCLUDED[/COLOR][/B][/SIZE]
+
+[IMG]https://i.imgur.com/u5tIlfT.png[/IMG]
+
+[SIZE="3"][B]DO NOT FLASH GAPPS, ALREADY INCLUDED[/B][/SIZE]
+
+[img]https://i.imgur.com/gMGb8ee.png[/img]
+
+[SIZE=5][B][COLOR=#0060FF]First Time Install / Clean Flash[/COLOR][/B][/SIZE]
+1. Download the proper flashable ZIP for your device
+2. Reboot to Recovery
+3. Wipe Data/Cache/System
+4. Format Data (Optional)
+5. Flash the ROM
+6. Reboot to System
+7. Reboot to Recovery (Optional)
+8. Flash Magisk (Optional)
+9. Reboot to System and [COLOR=#0060FF]#KeepEvolving[/COLOR]
+
+[SIZE=5][B][COLOR=#0060FF]Update / Dirty Flash[/COLOR][/B][/SIZE]
+1. Reboot to Recovery
+2. Download the proper flashable ZIP for your device
+3. Reboot to Recovery (Optional)
+4. Flash Magisk (Optional)
+5. Flash the ROM
+6. Reboot to System and [COLOR=#0060FF]#KeepEvolving[/COLOR]
+
+
+
+[URL='https://imgur.com/a/dQiYBql'][IMG]https://i.imgur.com/fmAkMrL.png[/IMG][/URL][URL='https://sourceforge.net/projects/evolution-x/files/##DEVICE_CODENAME##'][IMG]https://i.imgur.com/F2WF8Mx.png[/IMG][/URL]
+
+[URL='##DONATE_URL##'][IMG]https://i.imgur.com/JBON20U.png[/IMG][/URL][URL='https://t.me/EvolutionX'][IMG]https://i.imgur.com/9dKRGuj.png[/IMG][/URL][URL='https://t.me/EvolutionX##DEVICE_CODENAME##'][IMG]https://i.imgur.com/PZ6h5NN.png[/IMG][/URL]
+
+
+
+[SIZE=1][URL='https://imgur.com/gallery/SvmdfPb']Light Screenshots[/URL] / [URL='https://imgur.com/gallery/c3QKAdp']Dark Screenshots[/URL] / [URL='https://sourceforge.net/projects/evolution-x/files/##DEVICE_CODENAME##']Download[/URL]
+
+[URL='##DONATE_URL##']Donate to me![/URL] / [URL='https://t.me/EvolutionX']Official Chat[/URL] / [URL='https://t.me/EvolutionX##DEVICE_CODENAME##']Device Support[/URL][/SIZE]
+
+
+
+[hr][/hr]
+
+
+
+[IMG]https://i.imgur.com/LU9gsrQ.png[/IMG]
+
+
+[/CENTER]
+[LIST]
+[*][CENTER][B]Android OS version:[/B] 11.0_r20[/CENTER]
+[*][CENTER][B]Security patch level:[/B] November 2020[/CENTER]
+[*][CENTER][B]Build Author:[/B] [url="##XDA_URL##"]##NAME##[/url][/CENTER]
+[*][CENTER][B]Source Code:[/B] [url="https://github.com/Evolution-X"]https://github.com/Evolution-X[/url][/CENTER]
+[*][CENTER][B]Kernel Source Code:[/B] [url="##KERNEL_SOURCE_URL##"]##KERNEL_SOURCE_URL##[/url][/CENTER]
+[*][CENTER][B]ROM Developer:[/B] [URL='https://forum.xda-developers.com/member.php?u=4936496']joeyhuab[/URL][/CENTER]
+[/LIST]
+`;
